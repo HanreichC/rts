aeda7b85e742b06361c87a3e0c83c4c7e15887c9
Christopher Hanreich <chanreich@outlook.de>
2026-08-14 20:55:52 +0200

    feat: add ressource generation

--- rts/Scripts/Buildings/BuildingBase.cs
+++ rts/Scripts/Buildings/BuildingBase.cs
@@ -2,25 +2,53 @@ using Godot;
 using rts.Exceptions;
 using rts.scripts.Player;
 
+// ReSharper disable All
+
 namespace rts.scripts.Buildings;
 
 public partial class BuildingBase : Node3D
 {
-	[ExportGroup("Costs")] [Export] public PlayerRessourceType CostType { get; set; }
+	[ExportGroup("Building Costs")] [Export] public PlayerRessourceType CostType { get; set; }
 
 	[Export] public float CostValue { get; set; }
+
+	private PlayerRessource _currentCostPlayerRessource => PlayerEconomy.Instance.TryGetPlayerRessource(CostType);
 	
-	private PlayerRessource _currentPlayerRessource => PlayerEconomy.Instance.TryGetPlayerRessource(CostType);
+	[ExportGroup("Ressource Generation")] [Export] public PlayerRessourceType RessourceGenerationType { get; set; }
+	
+	[Export] public float RessourceGenerationValue { get; set; }
+	[Export] public float RessourceGenerationInterval { get; set; }
+	
+	private PlayerRessource _currentRessourceGenerationPlayerRessource => PlayerEconomy.Instance.TryGetPlayerRessource(RessourceGenerationType);
 
 	public void EnsureBuildRequirementsMet()
 	{
-		var currentPlayerRessource = _currentPlayerRessource;
-		
+		var currentPlayerRessource = _currentCostPlayerRessource;
+
 		if (currentPlayerRessource is null
-		    || !currentPlayerRessource.CanAfford(CostValue))
+			|| !currentPlayerRessource.CanAfford(CostValue))
 			throw new BuildRequirementsAreNotMetException();
 	}
 
 	public void SpendBuildRequirements()
-		=> _currentPlayerRessource.Spend(CostValue);
+		=> _currentCostPlayerRessource.Spend(CostValue);
+
+	public override void _Ready()
+	{
+		if (_currentRessourceGenerationPlayerRessource == null
+			|| RessourceGenerationValue <= 0
+			|| RessourceGenerationInterval <= 0)
+			return;
+		
+		var ressourceGenerationTimer = new Timer
+		{
+			WaitTime = RessourceGenerationInterval,
+			Autostart = true
+		};
+		AddChild(ressourceGenerationTimer);
+		ressourceGenerationTimer.Timeout += OnRessourceGenerationTimerTimeout;
+	}
+	
+	private void OnRessourceGenerationTimerTimeout()
+		=> _currentRessourceGenerationPlayerRessource.AddValue(RessourceGenerationValue);
 }
